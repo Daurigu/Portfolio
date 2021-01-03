add575d8bb786c84866053769f9a126b46bac63f
daurigu <daurigu@outlook.com>
2021-01-02 21:14:52 -0500

    CORS Added

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -46,11 +46,13 @@ INSTALLED_APPS = [
 
     #Third party
     'rest_framework'
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,3 +129,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#CORS_ORIGIN_WHITELIST = 'localhost:3000',
+CORS_ORIGIN_ALLOW_ALL = True
